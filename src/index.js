c8cae5d93a30f76c04e9329a634064163585cc2e
Mateusz <mateusz.pytel@grupakmk.pl>
2020-11-30 22:56:43 +0100

    eslinter added

--- src/index.js
+++ src/index.js
@@ -7,61 +7,61 @@ const MagentoConfigHelper = require('./utils/magento-config-helper');
  * @returns {{router: *, route: string, pluginName: string, domainName: string}}
  */
 module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
-    const createMage2RestClient = () => {
-        const client = getRestApiClient();
-        client.addMethods('mage_config', (restClient) => {
-            const module = {};
-            module.get = function () {
-                return restClient.get('/kmk-apistoreconfig/apiconfig/get');
-            };
+  const createMage2RestClient = () => {
+    const client = getRestApiClient();
+    client.addMethods('mage_config', (restClient) => {
+      const module = {};
+      module.get = function () {
+        return restClient.get('/kmk-apistoreconfig/apiconfig/get');
+      };
 
-            return module;
-        });
+      return module;
+    });
 
-        return client;
-    };
+    return client;
+  };
 
-    /**
+  /**
      * Returns map of mage configuration
      * per website and store
      */
-    router.get('/:websiteId/:storeId', async (req, res) => {
-        try {
-            const {websiteId, storeId} = req.params;
-            if (!websiteId) { throw new Error(`Website id is required`); }
-            if (!storeId) { throw new Error(`Store id is required`); }
+  router.get('/:websiteId/:storeId', async (req, res) => {
+    try {
+      const {websiteId, storeId} = req.params;
+      if (!websiteId) { throw new Error(`Website id is required`); }
+      if (!storeId) { throw new Error(`Store id is required`); }
 
-            let helper = new MagentoConfigHelper();
-            let config = await cache.getCacheInstance().get(helper.getCacheKey(websiteId, storeId));
+      let helper = new MagentoConfigHelper();
+      let config = await cache.getCacheInstance().get(helper.getCacheKey(websiteId, storeId));
 
-            if (!config) {
-                const client = createMage2RestClient();
-                config = await client.mage_config.get();
-                helper = new MagentoConfigHelper(config);
+      if (!config) {
+        const client = createMage2RestClient();
+        config = await client.mage_config.get();
+        helper = new MagentoConfigHelper(config);
 
-                const map = helper.getCacheReadyConfigurationMap();
-                map.forEach((value, key) => {
-                    cache.getCacheInstance().set(key, value, ['config']);
-                });
+        const map = helper.getCacheReadyConfigurationMap();
+        map.forEach((value, key) => {
+          cache.getCacheInstance().set(key, value, ['config']);
+        });
 
-                config = map.get(helper.getCacheKey(websiteId, storeId));
+        config = map.get(helper.getCacheKey(websiteId, storeId));
 
-                if (!output) {
-                    apiError(res, `Configuration was not found`);
-                    return;
-                }
-            }
-
-            apiStatus(res, config, 200);
-        } catch (err) {
-            apiError(res, err);
+        if (!config) {
+          apiError(res, `Configuration was not found`);
+          return;
         }
-    });
+      }
 
-    return {
-        domainName: '@grupakmk',
-        pluginName: 'magento-config',
-        route: '/config',
-        router
-    };
+      apiStatus(res, config, 200);
+    } catch (err) {
+      apiError(res, err);
+    }
+  });
+
+  return {
+    domainName: '@grupakmk',
+    pluginName: 'magento-config',
+    route: '/config',
+    router
+  };
 };
--- src/utils/magento-config-helper.js
+++ src/utils/magento-config-helper.js
@@ -1,58 +1,56 @@
 const isObject = require('lodash/isObject');
 
 class MagentoConfigHelper {
-
-    getConfigurationOfWebsite (websiteId) {
-        if (this.configuration.hasOwnProperty(websiteId)) {
-            return this.configuration[websiteId];
-        }
-
-        return null;
+  getConfigurationOfWebsite (websiteId) {
+    if (this.configuration.hasOwnProperty(websiteId)) {
+      return this.configuration[websiteId];
     }
 
-    getConfigurationOfStore (websiteId, storeId) {
-        const websiteConfiguration = this.getConfigurationOfWebsite(websiteId);
-        if (websiteConfiguration && websiteConfiguration.hasOwnProperty(storeId)) {
-            return websiteConfiguration[storeId];
-        }
+    return null;
+  }
 
-        return null;
+  getConfigurationOfStore (websiteId, storeId) {
+    const websiteConfiguration = this.getConfigurationOfWebsite(websiteId);
+    if (websiteConfiguration && websiteConfiguration.hasOwnProperty(storeId)) {
+      return websiteConfiguration[storeId];
     }
 
-    getCacheReadyConfigurationMap () {
-        const map = new Map();
-        Object.keys(this.configuration).forEach(websiteId => {
-            const configPerWebsite = this.configuration[websiteId];
-            Object.keys(configPerWebsite).forEach(storeId => {
-                const configPerStore = configPerWebsite[storeId];
-                const configKey = this.getCacheKey(websiteId, storeId);
-                map.set(configKey, configPerStore);
-            });
-        });
+    return null;
+  }
 
-        return map;
+  getCacheReadyConfigurationMap () {
+    const map = new Map();
+    Object.keys(this.configuration).forEach(websiteId => {
+      const configPerWebsite = this.configuration[websiteId];
+      Object.keys(configPerWebsite).forEach(storeId => {
+        const configPerStore = configPerWebsite[storeId];
+        const configKey = this.getCacheKey(websiteId, storeId);
+        map.set(configKey, configPerStore);
+      });
+    });
+
+    return map;
+  }
+
+  getCacheKey (websiteId, storeId) {
+    return `website_${websiteId}_store_${storeId}`;
+  }
+
+  getIdsFromKey (key) {
+
+  }
+
+  constructor (configuration) {
+    let conf = {};
+    if (configuration && isObject(configuration)) {
+      conf = configuration;
+    }
+    if (configuration instanceof Array && configuration.length > 0) {
+      conf = configuration[0];
     }
 
-    getCacheKey (websiteId, storeId) {
-        return `website_${websiteId}_store_${storeId}`;
-    }
-
-    getIdsFromKey (key) {
-        return
-    }
-
-    constructor (configuration) {
-        let conf = {};
-        if (configuration && isObject(configuration)) {
-            conf = configuration;
-        }
-        if (configuration instanceof Array && configuration.length > 0) {
-            conf = configuration[0];
-        }
-
-
-        this.configuration = conf;
-    }
+    this.configuration = conf;
+  }
 }
 
 module.exports = MagentoConfigHelper;
