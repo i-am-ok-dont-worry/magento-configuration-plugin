1c1f3cc7d0ab3932bfaafd910768690638a48c67
Mateusz <mateusz.pytel@grupakmk.pl>
2020-11-30 19:49:11 +0100

    install script update

--- bin/install.sh
+++ bin/install.sh
@@ -7,9 +7,9 @@ PLUGIN_NAME=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-COMPANY=$(cat package.json | grep name | sed 's/.*"name": "\(.*\)".*/\1/')
+COMPANY=$(cat package.json | grep company | sed 's/.*"company": "\(.*\)".*/\1/')
 echo "Installing LSF plugin" $PLUGIN_NAME from $COMPANY "..."
 
 mkdir -p "$cwd/../../vendor/@$COMPANY"
-cp -R src/ ../../vendor/@$COMPANY
+cp -R src/* ../../vendor/@$COMPANY
 cp README.md ../../vendor/@$COMPANY/README.md
