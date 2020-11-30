d0c2ccacffd340cb18707cd60b627e0986d81881
Mateusz <mateusz.pytel@grupakmk.pl>
2020-11-30 19:14:30 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = ({ router, apiStatus, apiError, cacheInstance }) => {
-    console.warn('----> Plugin initialized');
+const isObject = require('lodash/isObject');
+
+module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
+    const createMage2RestClient = () => {
+        const client = getRestApiClient();
+        client.addMethods('mage_config', (restClient) => {
+            const module = {};
+            module.get = function () {
+                return restClient.get('/kmk-apistoreconfig/apiconfig/get');
+            };
+
+            return module;
+        });
+
+        return client;
+    };
+
+    /**
+     * Returns map of mage configuration
+     */
+    router.get('/get', async (req, res) => {
+        const client = createMage2RestClient();
+        client.mage_config.get()
+            .then(config => {
+                const helper = new MagentoConfigHelper(config);
+                const map = helper.getCacheReadyConfigurationMap();
+                map.forEach((value, key) => {
+                    console.warn('Setting for key: ', key);
+                    cache.getCacheInstance().set(key, value, ['config']);
+                });
+            });
+
+        cache.get(req, ['mage-config'], client.mage_config.get)
+            .then(result => apiStatus(res, result, 200))
+            .catch(err => apiError(res, err));
+    });
+
+    router.get('/:websiteId/:storeId', async (req, res) => {
+        try {
+            const helper = new MagentoConfigHelper();
+            const {websiteId, storeId} = req.params;
+            const config = await cache.getCacheInstance().get(helper.getCacheKey(websiteId, storeId));
+            if (!config) {
+                apiError(res, `Configuration was not found`);
+                return;
+            }
+
+            apiStatus(res, config, 200);
+        } catch (err) {
+            apiError(res, err);
+        }
+    });
+
+    return {
+        domainName: '@grupakmk',
+        pluginName: 'magento-config',
+        route: '/config',
+        router
+    };
 };
---
+++ src/utils/magento-config-helper.js
@@ -0,0 +1,55 @@
+const isObject = require('lodash/isObject');
+
+class MagentoConfigHelper {
+
+    getConfigurationOfWebsite (websiteId) {
+        if (this.configuration.hasOwnProperty(websiteId)) {
+            return this.configuration[websiteId];
+        }
+
+        return null;
+    }
+
+    getConfigurationOfStore (websiteId, storeId) {
+        const websiteConfiguration = this.getConfigurationOfWebsite(websiteId);
+        if (websiteConfiguration && websiteConfiguration.hasOwnProperty(storeId)) {
+            return websiteConfiguration[storeId];
+        }
+
+        return null;
+    }
+
+    getCacheReadyConfigurationMap () {
+        const map = new Map();
+        Object.keys(this.configuration).forEach(websiteId => {
+            const configPerWebsite = this.configuration[websiteId];
+            Object.keys(configPerWebsite).forEach(storeId => {
+                const configPerStore = configPerWebsite[storeId];
+                const configKey = this.getCacheKey(websiteId, storeId);
+                map.set(configKey, configPerStore);
+            });
+        });
+
+        return map;
+    }
+
+    getCacheKey (websiteId, storeId) {
+        return `website_${websiteId}_store_${storeId}`;
+    }
+
+
+    constructor (configuration) {
+        let conf = {};
+        if (configuration && isObject(configuration)) {
+            conf = configuration;
+        }
+        if (configuration instanceof Array && configuration.length > 0) {
+            conf = configuration[0];
+        }
+
+
+        this.configuration = conf;
+    }
+}
+
+module.exports = MagentoConfigHelper;
